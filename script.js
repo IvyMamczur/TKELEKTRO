b4f33940fae96fa82fe93e10bac77adcebf4467d
IvyMamczur <ivamam002@gmail.com>
2025-08-06 02:29:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -109,11 +109,3 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector("nav ul");
-
-    menuToggle.addEventListener("click", () => {
-        navMenu.classList.toggle("show");
-    });
-});
